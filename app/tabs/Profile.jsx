d47c1e76d2092e7ade471e69c2cd4396be21f272
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-08-06 18:49:42 +0100

    profile

--- app/tabs/Profile.jsx
+++ app/tabs/Profile.jsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const Profile = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Profile</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
